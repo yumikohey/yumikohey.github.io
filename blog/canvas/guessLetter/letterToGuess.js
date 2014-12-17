0d46a403b04e81cfc50e7c1b3450976d5c80bfed
Yumiko Huang <dayumikoda@gmail.com>
2014-12-17 13:05:57 -0800

    letter to guess games first commit

--- blog/canvas/guessLetter/letterToGuess.js
+++ blog/canvas/guessLetter/letterToGuess.js
@@ -1,41 +1,39 @@
-window.addEventListener("load", eventWindowLoaded, false);
-
+window.addEventListener('load', eventWindowLoaded, false);
 var Debugger = function () { };
-
 Debugger.log = function (message) {
- try {
- console.log(message);
- } catch (exception) {
- return;
- }
+  try {
+    console.log(message);
+  } catch (exception) {
+    return;
+  }
 }
-
-function eventWindowLoaded () {
- canvasApp();
+function eventWindowLoaded() {
+  canvasApp();
 }
-
 function canvasSupport () {
- return Modernizr.canvas;
+    return Modernizr.canvas;
 }
+function eventWindowLoaded() {
+  canvasApp();
+}
+function canvasApp() {
+  var guesses = 0;
+  var message = "Guess The Letter From a (lower) to z (higher)";
+  var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var today = new Date();
+  var letterToGuess = "";
+  var higherOrLower = "";
+  var lettersGuessed;
+  var gameOver = false;
 
-var guesses = 0;
-var message="Guess The Letter From a (lower) to z (higher)";
-var letters = [
- "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
- "p","q","r","s","t","u","v","w","x","y","z"
- ];
-var today = new Date();
-var letterToGuess = "";
-var higherOrLower = "";
-var lettersGuessed;
-var gameOver = false;
-
-function canvasApp () {
   if (!canvasSupport()) {
-   return;
-   }
+       return;
+      }
+
   var theCanvas = document.getElementById("canvasOne");
   var context = theCanvas.getContext("2d");
+
+  initGame();
 
   function initGame() {
     var letterIndex = Math.floor(Math.random() * letters.length);
@@ -43,76 +41,87 @@ function canvasApp () {
     guesses = 0;
     lettersGuessed = [];
     gameOver = false;
-    window.addEventListener("keydown", eventKeyPressed, true);
+    window.addEventListener("keyup",eventKeyPressed,true);
+    var formElement = document.getElementById("createImageData");
+    formElement.addEventListener('click', createImageDataPressed, false);
     drawScreen();
   }
 
   function eventKeyPressed(e) {
-        if(!gameOver) {
-          // convert char code into letter
-          var letterPressed = String.fromCharCode(e.keyCode);
-          letterPressed = letterPressed.toLowerCase();
-          guesses++;
-          lettersGuessed.push(letterPressed);
-          if(letterPressed == letterToGuess) {
-            gameOver = true;
-          } else {
-            letterIndex = letters.indexOf(letterToGuess);
-            guessIndex = letters.indexOf(letterPressed);
-            Debugger.log(guessIndex);
-            if (guessIndex < 0) {
-              higherOrLower = "That is not a letter";
-            } else if (guessIndex > letterIndex) {
-              higherOrLower = "Lower";
-            } else {
-              higherOrLower = "Higher";
-            }
-          }
-          drawScreen();
+    if (!gameOver) {
+      var letterPressed = String.fromCharCode(e.keyCode);
+      letterPressed = letterPressed.toLowerCase();
+      guesses++;
+      lettersGuessed.push(letterPressed);
+
+      if (letterPressed == letterToGuess) {
+        gameOver = true;
+      } else {
+
+        letterIndex = letters.indexOf(letterToGuess);
+        guessIndex = letters.indexOf(letterPressed);
+        Debugger.log(guessIndex);
+        if (guessIndex < 0) {
+          higherOrLower = "That is not a letter";
+        } else if (guessIndex > letterIndex) {
+          higherOrLower = "Lower";
+        } else {
+          higherOrLower = "Higher";
         }
+
       }
+      drawScreen();
+      }
+  }
 
   function drawScreen() {
-          //Background
-          context.fillStyle = "#ffffaa";
-          context.fillRect(0, 0, 500, 300);
-          //Box
-          context.strokeStyle = "#000000";
-          context.strokeRect(5,5,490,290);
-          context.textBaseline = "top";
-
-          //Date
-          context.fillStyle = "#000000";
-          context.font = "10px Sans-Serif";
-          context.fillText(today, 150, 10);
-
-          //Message
-          context.fillStyle = "#FF0000";
-          context.font = "14px Sans-Serif";
-          context.fillText (message, 125, 30);
-          context.fillStyle = "#109910";
-          context.font = "16px Sans-Serif";
-          context.fillText ('Guesses: ' + guesses, 215,50);
-
-          //Higher Or Lower
-           context.fillStyle = "#000000";
-           context.font = "16px Sans-Serif";
-           context.fillText ("Higher Or Lower: " + higherOrLower, 150,125);
-           //Letters Guessed
-           context.fillStyle = "#FF0000";
-           context.font = "16px Sans-Serif";
-           context.fillText ("Letters Guessed: " + lettersGuessed.toString(), 10, 260);
-           if (gameOver) {
-             context.fillStyle = "#FF0000";
-             context.font = "40px Sans-Serif";
-             context.fillText ("You Got It!", 150, 180);
-           }
-      }
-
-    function createImageDataPressed(e) {
-         window.open(theCanvas.toDataURL(),"canvasImage","left=0,top=0,width=" +
-         theCanvas.width + ",height=" + theCanvas.height +",toolbar=0,resizable=0");
+    //Background
+    context.fillStyle = "aqua";
+    context.fillRect(0, 0, 500, 300);
+    context.globalAlpha = 0.1;
+    //Box
+    context.strokeStyle = "#000000";
+    context.strokeRect(5,  5, 490, 290);
+    context.textBaseline = "top";
+    //Date
+    context.globalAlpha = 0.5;
+    context.fillStyle    = "#000000";
+    context.font         = "10px Helvetica";
+    context.fillText  (today, 150 ,10);
+    //Message
+    context.globalAlpha = 1;
+    context.fillStyle    = "#109910";
+    context.font         = "14px Helvetica";
+    context.fillText  (message, 125, 30);
+    //Guesses
+    context.globalAlpha = 1;
+    context.fillStyle    = "#109910";
+    context.font         = "16px Helvetica";
+    context.fillText  ('Guesses: ' + guesses, 215, 50);
+    //Higher Or Lower
+    context.globalAlpha = 1;
+    context.fillStyle    = "#000000";
+    context.font         = "16px Helvetica";
+    context.fillText  ("Higher Or Lower: " + higherOrLower, 150,125);
+    //Letters Guessed
+    context.globalAlpha = 1;
+    context.fillStyle    = "#109910";
+    context.font         = "16px Helvetica";
+    context.fillText  ("Your guesses: " + lettersGuessed.toString(), 10, 260);
+    if (gameOver) {
+      context.fillStyle    = "#FFFFFF";
+      context.font         = "40px Helvetica";
+      context.fillText  ( "You Got it!", 150, 180);
     }
 
-      drawScreen();
+
+
+  }
+
+  function createImageDataPressed(e) {
+
+    window.open(theCanvas.toDataURL(),"canavsImage","left=0,top=0,width=" + theCanvas.width + ",height=" + theCanvas.height +",toolbar=0,resizable=0");
+
+  }
+
 }
