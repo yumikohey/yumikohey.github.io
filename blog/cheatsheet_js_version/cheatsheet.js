f50d014e432d9e33739123793a2f057d66962399
Yumiko Huang <dayumikoda@gmail.com>
2015-01-24 18:52:17 -0800

    json file attached

--- blog/cheatsheet_js_version/cheatsheet.js
+++ blog/cheatsheet_js_version/cheatsheet.js
@@ -1,29 +1,46 @@
-$title = $('.yellow-title');
-$propertyName = $('#propertyName');
-$red = $('.red');
-$blue = $('.blue');
-$dep = $('#dep');
-$totalCategory = 0;
-$totalType = 0;
+$section = $('section');
 
 function display(){    
-    $.getJSON('https://api.myjson.com/bins/2grsf', function(data) {
+    $.getJSON('https://http://yumikohey.github.io/blog/cheatsheet_js_version/data.json', function(data) {
         $totalCategory = data.info.length;
         $totalType = data.info[0].details.length;
-        console.log($totalCategory);
-        console.log($totalType);
+        $row = $("<div class='right-row'></div>");
+        $section.append($row);
+        $yellowTitle = $("<div class='column yellow-title'></div>");
         $titleText = data.info[0].category;
-        console.log($titleText);
-        $title.text($titleText);
-        $info = data.info[0].details[0][0];
-        $propertyText = $info.type;
-        $propertyName.text($propertyText);
-        $redText = $info.syntax[0];
-        $blueText = $info.syntax[1];
-        $red.text($redText);
-        $blue.text($blueText);
-        $depText = $info.description;
-        $dep.text($depText);
+        $yellowTitle.text($titleText);
+        $row.append($yellowTitle);
+        
+        for(var i = 0; i < $totalCategory; i++){
+           for(var j=0; j < $totalType; j++){
+            $column = $("<div class='column'></div>");
+            $colFour = $("<div class='col-4'></div>");
+            $propertyName = $("<p id='propertyName'></p>");
+            $info = data.info[i].details[j][0];
+            $propertyText = $info.type;
+            $propertyName.text($propertyText);
+            $colFour.append($propertyName);
+            $column.append($colFour);
+            $colEight = $("<div class='col-8'><p></p></div>");
+            $red = $("<span class='red'></span>");
+            $blue= $("<span class='blue'></span>");
+            $redText = $info.syntax[0];
+            $blueText = $info.syntax[1];
+            $red.text($redText);
+            $blue.text($blueText);
+            $colEight.append($red);
+            $colEight.append($blue);
+            $dep = $("<p id='dep'></p>");
+            $depText = $info.description;
+            $dep.text($depText);
+            $colEight.append($dep);
+            $column.append($colEight);
+            $row.append($column);
+           }
+        }
+        $columnHeight = $(window).height();
+        console.log($columnHeight);
+        $('.right-row').height($columnHeight);
     });
 }
 
