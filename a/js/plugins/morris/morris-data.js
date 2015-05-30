c817e14e0227197ad6bff87f4174a1bf0ee78494
Yumiko Huang <dayumikoda@gmail.com>
2015-05-29 23:56:54 -0700

    added OS ui

---
+++ a/js/plugins/morris/morris-data.js
@@ -0,0 +1,497 @@
+$(function(){
+	Morris.Donut({
+	  element: 'total_cpu',
+	  data: [
+	    {label: "Available CPU", value: 86},
+	    {label: "In use CPU", value: 14}
+	  ],
+	  formatter: function(y, data){
+	  	return y + '%'
+	  },
+	  resize:true,
+	});
+	Morris.Donut({
+	  element: 'total_ram',
+	  data: [
+	    {label: "Total RAM", value: 100},
+	    {label: "In use RAM", value: 20}
+	  ],
+	  formatter: function(y, data){
+	  	return y + 'GB'
+	  },
+	  resize:true,
+	});
+	Morris.Donut({
+	  element: 'total_capacity',
+	  data: [
+	    {label: "Available Capacity", value: 495},
+	    {label: "Used Capacity", value: 0}
+	  ],
+	  formatter: function(y, data){
+	  	return y + 'GB'
+	  },
+	  resize:true,
+	});
+});
+
+// Flot Chart Dynamic Chart
+
+$(function() {
+
+    var container = $("#cpu-line-chart");
+
+    // Determine how many data points to keep based on the placeholder's initial size;
+    // this gives us a nice high-res plot while avoiding more than one point per pixel.
+
+    var maximum = container.outerWidth() / 2 || 300;
+
+    //
+
+    var data = [];
+
+    function getRandomData() {
+
+        if (data.length) {
+            data = data.slice(1);
+        }
+
+        while (data.length < maximum) {
+            var previous = data.length ? data[data.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            data.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < data.length; ++i) {
+            res.push([i, data[i]])
+        }
+
+        return res;
+    }
+
+    //
+
+    var series = [{
+        data: getRandomData(),
+        lines: {
+            fill: true
+        },
+        color: '#7EC0EE'
+    }];
+    //
+
+    var plot = $.plot(container, series, {
+        grid: {
+            borderWidth: 1,
+            minBorderMargin: 20,
+            labelMargin: 10,
+            backgroundColor: {
+                colors: ["#fff", "#e4f4f4"]
+            },
+            margin: {
+                top: 8,
+                bottom: 20,
+                left: 20
+            },
+            markings: function(axes) {
+                var markings = [];
+                var xaxis = axes.xaxis;
+                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                    markings.push({
+                        xaxis: {
+                            from: x,
+                            to: x + xaxis.tickSize
+                        },
+                        color: "rgba(232, 232, 255, 0.2)"
+                    });
+                }
+                return markings;
+            }
+        },
+        xaxis: {
+            tickFormatter: function() {
+                return "";
+            }
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        legend: {
+            show: true
+        }
+    });
+
+    // Update the random dataset at 25FPS for a smoothly-animating chart
+
+    setInterval(function updateRandom() {
+        series[0].data = getRandomData();
+        plot.setData(series);
+        plot.draw();
+    }, 40);
+
+});
+
+$(function() {
+
+    var container = $("#ram-line-chart");
+
+    // Determine how many data points to keep based on the placeholder's initial size;
+    // this gives us a nice high-res plot while avoiding more than one point per pixel.
+
+    var maximum = container.outerWidth() / 2 || 300;
+
+    //
+
+    var data = [];
+
+    function getRandomData() {
+
+        if (data.length) {
+            data = data.slice(1);
+        }
+
+        while (data.length < maximum) {
+            var previous = data.length ? data[data.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            data.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < data.length; ++i) {
+            res.push([i, data[i]])
+        }
+
+        return res;
+    }
+
+    //
+
+    var series = [{
+        data: getRandomData(),
+        lines: {
+            fill: true
+        },
+        color: '#7EC0EE'
+    }];
+    //
+
+    var plot = $.plot(container, series, {
+        grid: {
+            borderWidth: 1,
+            minBorderMargin: 20,
+            labelMargin: 10,
+            backgroundColor: {
+                colors: ["#fff", "#e4f4f4"]
+            },
+            margin: {
+                top: 8,
+                bottom: 20,
+                left: 20
+            },
+            markings: function(axes) {
+                var markings = [];
+                var xaxis = axes.xaxis;
+                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                    markings.push({
+                        xaxis: {
+                            from: x,
+                            to: x + xaxis.tickSize
+                        },
+                        color: "rgba(232, 232, 255, 0.2)"
+                    });
+                }
+                return markings;
+            }
+        },
+        xaxis: {
+            tickFormatter: function() {
+                return "";
+            }
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        legend: {
+            show: true
+        }
+    });
+
+    // Update the random dataset at 25FPS for a smoothly-animating chart
+
+    setInterval(function updateRandom() {
+        series[0].data = getRandomData();
+        plot.setData(series);
+        plot.draw();
+    }, 40);
+
+});
+
+$(function() {
+
+    var container = $("#iops-line-chart");
+
+    // Determine how many data points to keep based on the placeholder's initial size;
+    // this gives us a nice high-res plot while avoiding more than one point per pixel.
+
+    var maximum = container.outerWidth() / 2 || 300;
+
+    //
+
+    var data = [];
+
+    function getRandomData() {
+
+        if (data.length) {
+            data = data.slice(1);
+        }
+
+        while (data.length < maximum) {
+            var previous = data.length ? data[data.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            data.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < data.length; ++i) {
+            res.push([i, data[i]])
+        }
+
+        return res;
+    }
+
+    var dataTwo = [];
+
+    function getRandomDataTwo() {
+
+        if (dataTwo.length) {
+            dataTwo = dataTwo.slice(1);
+        }
+
+        while (dataTwo.length < maximum) {
+            var previous = dataTwo.length ? dataTwo[dataTwo.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            dataTwo.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < dataTwo.length; ++i) {
+            res.push([i, dataTwo[i]])
+        }
+
+        return res;
+    }
+
+    //
+
+    var series = [{
+        data: getRandomData(),
+        lines: {
+            fill: true
+        },
+        color: '#68BE68'
+    },
+    	{
+        data: getRandomDataTwo(),
+        lines: {
+            fill: true
+        },
+        color: '#539853'
+    },
+
+    ];
+    //
+
+    var plot = $.plot(container, series, {
+        grid: {
+            borderWidth: 1,
+            minBorderMargin: 20,
+            labelMargin: 10,
+            backgroundColor: {
+                colors: ["#fff", "#e4f4f4"]
+            },
+            margin: {
+                top: 8,
+                bottom: 20,
+                left: 20
+            },
+            markings: function(axes) {
+                var markings = [];
+                var xaxis = axes.xaxis;
+                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                    markings.push({
+                        xaxis: {
+                            from: x,
+                            to: x + xaxis.tickSize
+                        },
+                        color: "rgba(232, 232, 255, 0.2)"
+                    });
+                }
+                return markings;
+            }
+        },
+        xaxis: {
+            tickFormatter: function() {
+                return "";
+            }
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        legend: {
+            show: true
+        }
+    });
+
+    // Update the random dataset at 25FPS for a smoothly-animating chart
+
+    setInterval(function updateRandom() {
+        series[0].data = getRandomData();
+        series[1].data = getRandomDataTwo();
+        plot.setData(series);
+        plot.draw();
+    }, 40);
+
+});
+
+$(function() {
+
+    var container = $("#bandwidth-line-chart");
+
+    // Determine how many data points to keep based on the placeholder's initial size;
+    // this gives us a nice high-res plot while avoiding more than one point per pixel.
+
+    var maximum = container.outerWidth() / 2 || 300;
+
+    //
+
+    var data = [];
+
+    function getRandomData() {
+
+        if (data.length) {
+            data = data.slice(1);
+        }
+
+        while (data.length < maximum) {
+            var previous = data.length ? data[data.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            data.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < data.length; ++i) {
+            res.push([i, data[i]])
+        }
+
+        return res;
+    }
+
+    var dataTwo = [];
+
+    function getRandomDataTwo() {
+
+        if (dataTwo.length) {
+            dataTwo = dataTwo.slice(1);
+        }
+
+        while (dataTwo.length < maximum) {
+            var previous = dataTwo.length ? dataTwo[dataTwo.length - 1] : 50;
+            var y = previous + Math.random() * 10 - 5;
+            dataTwo.push(y < 0 ? 0 : y > 100 ? 100 : y);
+        }
+
+        // zip the generated y values with the x values
+
+        var res = [];
+        for (var i = 0; i < dataTwo.length; ++i) {
+            res.push([i, dataTwo[i]])
+        }
+
+        return res;
+    }
+
+    //
+
+    var series = [{
+        data: getRandomData(),
+        lines: {
+            fill: true
+        },
+        color: '#68BE68'
+    },
+    	{
+        data: getRandomDataTwo(),
+        lines: {
+            fill: true
+        },
+        color: '#539853'
+    },
+
+    ];
+    //
+
+    var plot = $.plot(container, series, {
+        grid: {
+            borderWidth: 1,
+            minBorderMargin: 20,
+            labelMargin: 10,
+            backgroundColor: {
+                colors: ["#fff", "#e4f4f4"]
+            },
+            margin: {
+                top: 8,
+                bottom: 20,
+                left: 20
+            },
+            markings: function(axes) {
+                var markings = [];
+                var xaxis = axes.xaxis;
+                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                    markings.push({
+                        xaxis: {
+                            from: x,
+                            to: x + xaxis.tickSize
+                        },
+                        color: "rgba(232, 232, 255, 0.2)"
+                    });
+                }
+                return markings;
+            }
+        },
+        xaxis: {
+            tickFormatter: function() {
+                return "";
+            }
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        legend: {
+            show: true
+        }
+    });
+
+    // Update the random dataset at 25FPS for a smoothly-animating chart
+
+    setInterval(function updateRandom() {
+        series[0].data = getRandomData();
+        series[1].data = getRandomDataTwo();
+        plot.setData(series);
+        plot.draw();
+    }, 40);
+
+});
