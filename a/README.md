4fdeae4c800b505c13c4728c29212af928deeeea
Yumiko Huang <dayumikoda@gmail.com>
2015-06-10 12:37:39 -0700

    updated fortissimo site

--- a/README.md
+++ a/README.md
@@ -1,1 +1,1 @@
-![screen shot 042615](https://www.dropbox.com/s/s23pgh8qzamqs5n/Screenshot%202015-05-30%2000.07.43.png)
+![screen shot 042615](https://github.com/yumikohey/fortissimo/blob/master/images/fort_init.png)
--- a/js/pie.js
+++ a/js/pie.js
@@ -1,5 +1,18 @@
-$(function () {
-    var data = [{
+$(function(){
+
+    var pieGenerator = function(data, variable_id){
+        var options = {
+            animation: true
+        };
+        //Get the context of the canvas element we want to select
+        var c = $(variable_id);
+        var ct = c.get(0).getContext('2d');
+        var ctx = document.getElementById("pieChart").getContext("2d");
+        /*************************************************************************/
+        myNewChart = new Chart(ct).Pie(data, options);
+    }
+
+    var first_pie_data = [{
         label: 'Minimum OS Memory',
         value: 30,
         color: "#68BE68"
@@ -7,24 +20,13 @@ $(function () {
         label: 'Posix Share Memory',
         value: 50,
         color: "#539853"
-    }
+    }];
 
-    ]
+    var first_pie_chart_id = '#pieChart';
+    pieGenerator(first_pie_data, first_pie_chart_id);
 
-    var options = {
-        animation: true
-    };
 
-    //Get the context of the canvas element we want to select
-    var c = $('#pieChart');
-    var ct = c.get(0).getContext('2d');
-    var ctx = document.getElementById("pieChart").getContext("2d");
-    /*************************************************************************/
-    myNewChart = new Chart(ct).Pie(data, options);
-});
-
-$(function () {
-    var data = [{
+    var second_pie_data = [{
         label: 'Max Volume',
         value: 50,
         color: "#68BE68"
@@ -32,18 +34,9 @@ $(function () {
         label: 'Min Volume',
         value: 20,
         color: "#539853"
-    }
+    }];
 
-    ]
+    var second_pie_chart_id = '#pieChart_volume';
+    pieGenerator(second_pie_data, second_pie_chart_id);
+})
 
-    var options = {
-        animation: true
-    };
-
-    //Get the context of the canvas element we want to select
-    var c = $('#pieChart_volume');
-    var ct = c.get(0).getContext('2d');
-    var ctx = document.getElementById("pieChart").getContext("2d");
-    /*************************************************************************/
-    myNewChart = new Chart(ct).Pie(data, options);
-});
